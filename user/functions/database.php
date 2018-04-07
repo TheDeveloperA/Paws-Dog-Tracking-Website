117b9a188069bc3882344581ec4dcb516f6bb2c2
Mohammed Azhar Habib <TheDeveloperA@gmail.com>
2018-04-07 14:31:33 +0100

    Added methods for table

---
+++ user/functions/database.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: A's
+ * Date: 26/03/2018
+ * Time: 17:26
+ *
+ * This file is used to store all the functions used to communicate with DB.
+ */
+
+// Contains information to connect to DB.
+include ("../includes/config.php");
+
+
+    /**
+     * @param $email_address - The email address of the User, where the dog is saved to.
+     * @return array - Returns array of dogs registered to user.
+     */
+    function getDogs($email_address){
+
+        global $db;
+
+        $sql_query = "SELECT * FROM dogs WHERE dogs_user_email = '$email_address' ";
+        $result = mysqli_query($db,$sql_query);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+        $dogs_id = $row["dogs_id"];
+        $dogs_user_email = $row["dogs_user_email"];
+        $dogs_name = $row["dogs_name"];
+        $dogs_age = $row["dogs_age"];
+        $dogs_weight = $row["dogs_weight"];
+
+        return array($dogs_id,$dogs_user_email,$dogs_name,$dogs_age,$dogs_weight);
+    }
+
+    /**
+     * @param $id - The ID of the dog, used to retrieve all data for the dog
+     * @return array - Returns array of data for the dog.
+     */
+    function getDogsInfo($id){
+
+        global $db;
+
+        $sql_query = "SELECT * FROM dogs_info WHERE dogs_info_dogs_identifier = '$id'";
+        $result = mysqli_query($db,$sql_query);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+        $dogs_info_id = $row["dogs_info_id"];
+        $dogs_info_dogs_identifier = $row["dogs_info_dogs_identifier"];
+        $dogs_hourly_average = $row["dogs_hourly_average"];
+        $dogs_min_play = $row["dogs_min_play"];
+        $dogs_min_active = $row["dogs_min_active"];
+        $dogs_min_rest = $row["dogs_min_rest"];
+        $dogs_date = $row["dogs_min_date"];
+
+        return array($dogs_info_id,$dogs_info_dogs_identifier,$dogs_hourly_average,$dogs_min_play,$dogs_min_active,
+            $dogs_min_rest,$dogs_date);
+    }
